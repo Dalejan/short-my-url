f3de0c66874de5bb598e68dd3730acfc6fa373a2
Dalejan <david1804997@gmail.com>
2020-04-11 02:57:20 -0500

    Updates

---
+++ precache-manifest.b930c4d185f3a5f234e826f62431c0f6.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "257d21bef694052cb919e3711710dc9f",
+    "url": "/short-my-url/index.html"
+  },
+  {
+    "revision": "5f673e8ec6fdea66d1f2",
+    "url": "/short-my-url/static/css/main.a9e87b81.chunk.css"
+  },
+  {
+    "revision": "05f8bdcb1750bc8d479e",
+    "url": "/short-my-url/static/js/2.69ab51c2.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/short-my-url/static/js/2.69ab51c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f673e8ec6fdea66d1f2",
+    "url": "/short-my-url/static/js/main.36058e4c.chunk.js"
+  },
+  {
+    "revision": "f5317b2bb25c8f813282",
+    "url": "/short-my-url/static/js/runtime-main.e5b04023.js"
+  },
+  {
+    "revision": "c23534acbeddbaadfd0ab2d2bbfdfc84",
+    "url": "/short-my-url/static/media/Poppins-Bold.c23534ac.ttf"
+  },
+  {
+    "revision": "6b78c7ec468eb0e13c6c5c4c39203986",
+    "url": "/short-my-url/static/media/Poppins-ExtraBold.6b78c7ec.ttf"
+  },
+  {
+    "revision": "8ea2657f263b6b2a404bfb57f4160013",
+    "url": "/short-my-url/static/media/Poppins-ExtraLight.8ea2657f.ttf"
+  },
+  {
+    "revision": "41e8dead03fb979ecc23b8dfb0fef627",
+    "url": "/short-my-url/static/media/Poppins-Regular.41e8dead.ttf"
+  },
+  {
+    "revision": "dc8b886b4cf2a2426a6664ad17ebf7f9",
+    "url": "/short-my-url/static/media/bg-boost-desktop.dc8b886b.svg"
+  },
+  {
+    "revision": "47ef0c281d9e90c932dfd63faa38b94f",
+    "url": "/short-my-url/static/media/bg-boost-mobile.47ef0c28.svg"
+  },
+  {
+    "revision": "4a7a7f615fb37565a0a706a7379dfb7e",
+    "url": "/short-my-url/static/media/bg-shorten-mobile.4a7a7f61.svg"
+  },
+  {
+    "revision": "1922b68a4d1dce29b2c40904cdbc1bba",
+    "url": "/short-my-url/static/media/icon-brand-recognition.1922b68a.svg"
+  },
+  {
+    "revision": "e3d9caa9df26426d598106f7f7289625",
+    "url": "/short-my-url/static/media/icon-detailed-records.e3d9caa9.svg"
+  },
+  {
+    "revision": "7febcff9f143b83553d544fc04fc5b09",
+    "url": "/short-my-url/static/media/icon-facebook.7febcff9.svg"
+  },
+  {
+    "revision": "cc099cea0da2f5ae13aef182c107a854",
+    "url": "/short-my-url/static/media/icon-fully-customizable.cc099cea.svg"
+  },
+  {
+    "revision": "996b697c33ab8772e59e3c9bec1124ea",
+    "url": "/short-my-url/static/media/icon-instagram.996b697c.svg"
+  },
+  {
+    "revision": "c557c3d4e80b5eda1ae120c8c2257e26",
+    "url": "/short-my-url/static/media/icon-pinterest.c557c3d4.svg"
+  },
+  {
+    "revision": "cb4fea0b4771a5dfc70f787d5e01c91e",
+    "url": "/short-my-url/static/media/icon-twitter.cb4fea0b.svg"
+  },
+  {
+    "revision": "4c5b00dedce3ee3793849b33173e3765",
+    "url": "/short-my-url/static/media/illustration_afro.4c5b00de.svg"
+  }
+]);
